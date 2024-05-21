406ad1ff42a7131644cef1066ffdaeb9720c8a15
unknown <amr_elngm6@yahoo.com>
2024-05-21 09:32:38 +0300

    .

--- beats-help/templates/free-beat-upload-page.php
+++ beats-help/templates/free-beat-upload-page.php
@@ -336,27 +336,6 @@ if ( $new_product ) {
                     <div class="dokan-form-group dokan-new-cat-ui-title">
                         <label for="product_cat" class="form-label"><?php esc_html_e( 'Genre', 'dokan-lite' ); ?></label>
                     </div>
-                    <span class="dokan-add-new-cat-box cat_box_for_<?php echo esc_attr( $from ); ?>">
-                        <?php foreach ( $chosen_cat as $key => $term_id ) : ?>
-                            <div data-activate="no" class="dokan-select-product-category-container dokan_select_cat_for_<?php echo esc_attr( $from ); ?>_<?php echo esc_attr( $key ); ?>">
-                                <div data-selectfor="<?php echo esc_attr( $from ); ?>" class="dokan-form-group dokan-select-product-category dokan-category-open-modal" data-dokansclevel="<?php echo esc_attr( $key ); ?>" id="dokan-category-open-modal">
-                                    <span id="dokan_product_cat_res" class="dokan-select-product-category-title dokan-ssct-level-<?php echo esc_attr( $key ); ?>"><?php echo empty( $term_id ) ? __( '- Select a category -', 'dokan-lite' ) : Helper::get_ancestors_html( $term_id ); ?></span>
-                                    <span class="dokan-select-product-category-icon"><i class="fas fa-edit"></i></span>
-
-                                </div>
-                                <?php if ( ! $is_single ) : ?>
-                                    <div class="dokan-select-product-category-remove-container">
-                                        <span class="dokan-select-product-category-remove"><i class="fas fa-times"></i></span>
-                                    </div>
-                                <?php endif; ?>
-                                <span class="dokan-cat-inputs-holder dokan-cih-level-<?php echo esc_attr( $key ); ?>" >
-                                    <?php if ( ! empty( $term_id ) ) : ?>
-                                        <input data-field-name="chosen_product_cat" type="hidden" class="dokan_chosen_product_cat dokan_chosen_product_cat_<?php echo esc_attr( $term_id ); ?>" name="chosen_product_cat[]" value="<?php echo esc_attr( $term_id ); ?>"/>
-                                    <?php endif; ?>
-                                </span>
-                            </div>
-                        <?php endforeach; ?>
-                    </span>
 
 
             </div>
