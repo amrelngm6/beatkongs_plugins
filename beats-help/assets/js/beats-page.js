e6c1dc167a92e022044e0156e74f68303a317397
unknown <amr_elngm6@yahoo.com>
2024-05-21 22:43:21 +0300

    .

--- beats-help/assets/js/beats-page.js
+++ beats-help/assets/js/beats-page.js
@@ -9,43 +9,52 @@
             })   
         }
        
-        var btn = $('.open-modal');
-        var modal = $(btn.data('modal'));
-        
-        var span = $('.fbu-close');
-        var categoryInput = $(btn.data('input'));
-        var categoryText = $(btn.data('text'));
-        
-        console.log(modal)
-        console.log(categoryInput)
-        console.log(categoryText)
-        // Open the modal
-        btn.on('click', function() {
-            console.log(this)
-            modal.show();
+        $('#open-category-modal').on('click', function() {
+            t (this);
         });
-    
-        // Close the modal
-        span.on('click', function() {
-            modal.hide();
+
+        $('#open-station-modal').on('click', function() {
+            t (this);
         });
-    
-        // Close the modal when clicking outside of the modal content
-        $(window).on('click', function(event) {
-            if ($(event.target).is(modal)) {
+
+        function t (btn)
+        {
+            var modal = $(btn.data('modal'));
+            var span = $('.fbu-close');
+            var categoryInput = $(btn.data('input'));
+            var categoryText = $(btn.data('text'));
+            
+            console.log(modal)
+            console.log(categoryInput)
+            console.log(categoryText)
+            // Open the modal
+            btn.on('click', function() {
+                console.log(this)
+                modal.show();
+            });
+        
+            // Close the modal
+            span.on('click', function() {
                 modal.hide();
-            }
-        });
-    
-        // Handle category selection
-        // Handle category selection
-        jQuery(document).on('click', 'li.choose-genre', function() {
-            console.log(this)
-            var selectedCategory = $(this).text();
-            categoryInput.val(selectedCategory);
-            categoryText.html(selectedCategory);
-            modal.hide();
-        });
+            });
+        
+            // Close the modal when clicking outside of the modal content
+            $(window).on('click', function(event) {
+                if ($(event.target).is(modal)) {
+                    modal.hide();
+                }
+            });
+        
+            // Handle category selection
+            // Handle category selection
+            jQuery(document).on('click', 'li.choose-genre', function() {
+                console.log(this)
+                var selectedCategory = $(this).text();
+                categoryInput.val(selectedCategory);
+                categoryText.html(selectedCategory);
+                modal.hide();
+            });
+        }
     });
 })(jQuery);
 
