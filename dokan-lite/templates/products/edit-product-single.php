ed54a63523fb2cab5966fc5be27d0b8de5397aa7
unknown <amr_elngm6@yahoo.com>
2024-05-21 07:16:52 +0300

    .

--- dokan-lite/templates/products/edit-product-single.php
+++ dokan-lite/templates/products/edit-product-single.php
@@ -467,7 +467,7 @@ do_action( 'dokan_dashboard_wrap_before', $post, $post_id );
                             <?php do_action( 'dokan_new_product_form', $post, $post_id ); ?>
                             <?php do_action( 'dokan_product_edit_after_main', $post, $post_id ); ?>
 
-                            <?php do_action( 'dokan_product_edit_after_inventory_variants', $post, $post_id ); ?>
+                            <?php //do_action( 'dokan_product_edit_after_inventory_variants', $post, $post_id ); ?>
 
                             <?php if ( $post_id ) : ?>
                                 <?php do_action( 'dokan_product_edit_after_options', $post_id ); ?>
