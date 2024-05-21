43dba7db10ebf3e46cb93518492a42950ce358b6
unknown <amr_elngm6@yahoo.com>
2024-05-21 07:51:23 +0300

    .

--- beats-help/templates/beats-page.php
+++ beats-help/templates/beats-page.php
@@ -13,7 +13,7 @@ do_action('dokan_dashboard_wrap_start');
 
 // Display the main content of the beats page
 ?>
-<div id="dokan-beats-page-root"></div>
+<!-- <div id="dokan-beats-page-root"></div> -->
 <?php
 
 
