07dc722e7da9c635f3c65789dbadc2ed9c5c91ea
unknown <amr_elngm6@yahoo.com>
2024-05-21 10:16:43 +0300

    .

--- beats-help/templates/free-beat-upload-page.php
+++ beats-help/templates/free-beat-upload-page.php
@@ -310,7 +310,93 @@ if ( $new_product ) {
         <div id="fbu-category-modal" class="fbu-modal">
             <div class="fbu-modal-content">
 
-                        
+            <div class="current" data-kt-stepper-element="content">
+    <!--begin::Wrapper-->
+    <div class="w-100">
+        <!--begin::Heading-->
+        <div class="mb-13">
+            <!--begin::Title-->
+            <h2 class="mb-3">Deal Type</h2>
+            <!--end::Title-->
+
+            <!--begin::Description-->
+            <div class="text-muted fw-semibold fs-5">
+                If you need more info, please check out
+                <a href="#" class="link-primary fw-bold">FAQ Page</a>.
+            </div>
+            <!--end::Description-->
+        </div>
+        <!--end::Heading-->
+
+        <!--begin::Input group-->
+        <div class="fv-row mb-15 fv-plugins-icon-container" data-kt-buttons="true" data-kt-initialized="1">
+            <!--begin::Option-->
+            <label class="btn btn-outline btn-outline-dashed btn-active-light-primary d-flex text-start p-6 mb-6 active">
+                <!--begin::Input-->
+                <input class="btn-check" type="radio" checked="checked" name="offer_type" value="1">
+                <!--end::Input-->
+
+                <!--begin::Label-->
+                <span class="d-flex">
+                    <!--begin::Icon-->
+                    <i class="ki-duotone ki-profile-circle fs-3hx"><span class="path1"></span><span class="path2"></span><span class="path3"></span></i>                    <!--end::Icon-->
+
+                    <!--begin::Info-->
+                    <span class="ms-4">
+                        <span class="fs-3 fw-bold text-gray-900 mb-2 d-block">Personal Deal</span>
+
+                        <span class="fw-semibold fs-4 text-muted">
+                            If you need more info, please check it out
+                        </span>
+                    </span>
+                    <!--end::Info-->
+                </span>
+                <!--end::Label-->
+            </label>
+            <!--end::Option-->
+
+            <!--begin::Option-->
+            <label class="btn btn-outline btn-outline-dashed btn-active-light-primary d-flex text-start p-6">
+                <!--begin::Input-->
+                <input class="btn-check" type="radio" name="offer_type" value="2">
+                <!--end::Input-->
+
+                <!--begin::Label-->
+                <span class="d-flex">
+                    <!--begin::Icon-->
+                    <i class="ki-duotone ki-element-11 fs-3hx"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span></i>                    <!--end::Icon-->
+
+                    <!--begin::Info-->
+                    <span class="ms-4">
+                        <span class="fs-3 fw-bold text-gray-900 mb-2 d-block">Corporate Deal</span>
+
+                        <span class="fw-semibold fs-4 text-muted">
+                            Create corporate account to manage users
+                        </span>
+                    </span>
+                    <!--end::Info-->
+                </span>
+                <!--end::Label-->
+            </label>
+            <!--end::Option-->
+        <div class="fv-plugins-message-container fv-plugins-message-container--enabled invalid-feedback"></div></div>
+        <!--end::Input group-->
+
+        <!--begin::Actions-->
+        <div class="d-flex justify-content-end">
+            <button type="button" class="btn btn-lg btn-primary" data-kt-element="type-next">
+                <span class="indicator-label">
+                    Offer Details
+                </span>
+                <span class="indicator-progress">
+                    Please wait... <span class="spinner-border spinner-border-sm align-middle ms-2"></span>
+                </span>
+            </button>
+        </div>
+        <!--end::Actions-->
+    </div>
+    <!--end::Wrapper-->
+</div>
                 <!-- Modal content -->
                 <div class="dokan-product-category-modal-content">
                     <div class="dokan-product-category-modal-header">
