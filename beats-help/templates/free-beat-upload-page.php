4b80f87067858474ada959201ac623a687813eeb
unknown <amr_elngm6@yahoo.com>
2024-05-21 23:53:44 +0300

    .

--- beats-help/templates/free-beat-upload-page.php
+++ beats-help/templates/free-beat-upload-page.php
@@ -131,12 +131,12 @@ if ( $new_product ) {
                                 </div>
                             <?php endif; ?>
                         <?php endif; ?>
-                        <div class="flex">
+                        <div class="flex  open-modal cursor-pointer" id="open-category-modal" data-modal="#fbu-category-modal" data-text="#fbu-category-text" data-input="#fbu-category">
                             <div  class="w-full dokan-form-group dokan-select-product-category dokan-category-open-modal" data-dokansclevel="0"  id="fbu-category-g">
                                 <label for="fbu-category" class="font-semibold form-label">Category:</label>
                                 <div id="dokan_product_cat_res" class="dokan-select-product-category-title dokan-ssct-level-0"><span class="dokan-selected-category-product dokan-cat-selected" id="fbu-category-text"><span>Select Genre</span></span></div>
                             </div>
-                            <div  class="w-full  open-modal cursor-pointer" id="open-category-modal" data-modal="#fbu-category-modal" data-text="#fbu-category-text" data-input="#fbu-category">
+                            <div  class="w-full " >
                                 <span class="dokan-select-product-category-icon"><i class="fas fa-edit"></i></span>
                             </div>
                             <input type="hidden" id="fbu-category" name="fbu-category" readonly required>
