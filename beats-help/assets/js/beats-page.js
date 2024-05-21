3e5a6739ce0b4d00148d15577c96120c7b93dba9
unknown <amr_elngm6@yahoo.com>
2024-05-22 00:46:00 +0300

    .

--- beats-help/assets/js/beats-page.js
+++ beats-help/assets/js/beats-page.js
@@ -76,7 +76,7 @@
                      var gallery_ids = new Array();
                      var my_index = 0;
                      selection.each(function(attachment) {
-                        console.log(attachment)
+                        console.log(attachment.attributes.url)
                         gallery_ids[my_index] = attachment['id'];
                         my_index++;
                      });
