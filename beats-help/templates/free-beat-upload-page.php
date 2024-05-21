dd2deedafe107d0788f2848962d021433ee702ab
unknown <amr_elngm6@yahoo.com>
2024-05-21 21:22:57 +0300

    .

--- beats-help/templates/free-beat-upload-page.php
+++ beats-help/templates/free-beat-upload-page.php
@@ -307,7 +307,7 @@ if ( $new_product ) {
         do_action( 'dokan_dashboard_content_after' );
         
         
-        do_action( 'dokan_beats_category_popup' );
+        do_action( 'category_popup' );
         ?>
         <!-- Modal Popup for Category Selection -->
         
