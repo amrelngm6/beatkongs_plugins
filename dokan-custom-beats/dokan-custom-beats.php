ffecbc90905dd74d9d50c45428f2bb33613a60e7
unknown <amr_elngm6@yahoo.com>
2024-05-21 03:19:03 +0300

    .

--- dokan-custom-beats/dokan-custom-beats.php
+++ dokan-custom-beats/dokan-custom-beats.php
@@ -10,6 +10,7 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit; // Exit if accessed directly
 }
 
+// Initialize the plugin when all plugins are loaded
 add_action( 'plugins_loaded', 'dokan_custom_product_view_init' );
 
 function dokan_custom_product_view_init() {
@@ -17,23 +18,40 @@ function dokan_custom_product_view_init() {
         return;
     }
 
+    // Hook to add custom menu item to Dokan vendor dashboard
+    add_filter( 'dokan_get_dashboard_nav', 'dokan_custom_product_view_sidebar_menu', 10, 1 );
+
+    // Hook to load custom template
     add_action( 'dokan_load_custom_template', 'dokan_custom_product_view_load_template' );
-    add_action( 'dokan_render_settings_content', 'dokan_custom_product_view_sidebar_menu' );
+}
 
-    function dokan_custom_product_view_sidebar_menu() {
-        ?>
-        <li class="custom-product-view">
-            <a href="<?php echo esc_url( dokan_get_navigation_url( 'custom-product-view' ) ); ?>">
-                <?php _e( 'Custom Product View', 'dokan' ); ?>
-            </a>
-        </li>
-        <?php
-    }
+// Function to add custom menu item
+function dokan_custom_product_view_sidebar_menu( $urls ) {
+    $urls['custom-product-view'] = array(
+        'title' => __( 'Custom Product View', 'dokan' ),
+        'icon'  => '<i class="fas fa-eye"></i>',
+        'url'   => dokan_get_navigation_url( 'custom-product-view' ),
+        'pos'   => 150,
+    );
+    return $urls;
+}
 
-    function dokan_custom_product_view_load_template( $query_vars ) {
-        if ( isset( $query_vars['custom-product-view'] ) ) {
-            require_once __DIR__ . '/templates/custom-product-view.php';
-            exit;
-        }
+// Function to load custom template
+function dokan_custom_product_view_load_template( $query_vars ) {
+    if ( isset( $query_vars['custom-product-view'] ) ) {
+        add_action( 'dokan_dashboard_content_before', 'dokan_custom_product_view_content' );
+        add_action( 'dokan_dashboard_content_after', 'dokan_custom_product_view_wrap_end' );
+        remove_action( 'dokan_dashboard_content_before', 'dokan_account_migration_banner' );
     }
 }
+
+// Function to display custom content
+function dokan_custom_product_view_content() {
+    include __DIR__ . '/templates/custom-product-view.php';
+}
+
+// Function to end custom content wrap
+function dokan_custom_product_view_wrap_end() {
+    // Optional: Add any closing HTML tags if needed
+}
+?>
--- dokan-custom-beats/templates/custom-product-view.php
+++ dokan-custom-beats/templates/custom-product-view.php
@@ -25,3 +25,52 @@ if ( ! dokan_is_seller_enabled( $current_user->ID ) ) {
         </article>
     </div>
 </div>
+
+
+<div class="dokan-dashboard-wrap">
+    <?php dokan_get_template( 'global/seller-dashboard-nav.php', array( 'active_menu' => 'custom-product-view' ) ); ?>
+    <div class="dokan-dashboard-content dokan-product-listing">
+        <article class="dokan-product-listing-area">
+            <h1><?php _e( 'Custom Product View', 'dokan' ); ?></h1>
+            <p><?php _e( 'Here you can view your products in a custom layout.', 'dokan' ); ?></p>
+            
+            <!-- Your custom product view code goes here -->
+            <?php
+            $vendor_id = dokan_get_current_user_id();
+            $args = array(
+                'post_type'   => 'product',
+                'author'      => $vendor_id,
+                'post_status' => 'publish',
+                'posts_per_page' => -1,
+            );
+
+            $products = new WP_Query( $args );
+
+            if ( $products->have_posts() ) :
+                echo '<ul class="custom-product-list">';
+                while ( $products->have_posts() ) : $products->the_post();
+                    global $product;
+                    ?>
+                    <li>
+                        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                        <?php if ( has_post_thumbnail() ) : ?>
+                            <div class="product-thumbnail">
+                                <?php the_post_thumbnail(); ?>
+                            </div>
+                        <?php endif; ?>
+                        <div class="product-price">
+                            <?php echo $product->get_price_html(); ?>
+                        </div>
+                    </li>
+                    <?php
+                endwhile;
+                echo '</ul>';
+            else :
+                echo '<p>' . __( 'No products found', 'dokan' ) . '</p>';
+            endif;
+
+            wp_reset_postdata();
+            ?>
+        </article>
+    </div>
+</div>
