f0a90a8f9b7458223f64b682e39cd63f54474131
unknown <amr_elngm6@yahoo.com>
2024-05-21 03:40:46 +0300

    .

--- beats-help/templates/help-page.php
+++ beats-help/templates/help-page.php
@@ -2,9 +2,15 @@
 if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly
 }
-get_header();
+
+// Load Dokan's vendor dashboard header
+do_action('dokan_dashboard_wrap_start');
+
+// Display the main content of the help page
 ?>
 <div id="dokan-help-page-root"></div>
-
 <?php
-get_footer();
+
+// Load Dokan's vendor dashboard footer
+do_action('dokan_dashboard_wrap_end');
+?>
