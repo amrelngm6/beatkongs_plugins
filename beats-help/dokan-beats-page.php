3c269d4036303c8b692e3548ae479302e9ef404f
unknown <amr_elngm6@yahoo.com>
2024-05-21 10:08:04 +0300

    .

--- beats-help/dokan-beats-page.php
+++ beats-help/dokan-beats-page.php
@@ -70,7 +70,8 @@ function enqueue_dokan_beats_page_scripts() {
      // Enqueue CSS
      wp_enqueue_style('dokan-beats-page-css', plugin_dir_url(__FILE__) . 'assets/css/custom-style.css');
      wp_enqueue_script('dokan-beats-page-js', plugin_dir_url(__FILE__) . 'assets/js/beats-page.js', array('wp-element'), '1.0', true);
-     wp_enqueue_style('dokan-beats-page-css', plugin_dir_url(__FILE__) . 'assets/css/beats-page.css', array('wp-element'), '1.0', true);
+     wp_enqueue_style('dokan-beats-page-css2', plugin_dir_url(__FILE__) . 'assets/css/beats-page.css', array('wp-element'), '1.0', true);
+     wp_enqueue_style('beats-tailwind-page-css', plugin_dir_url(__FILE__) . 'assets/css/tailwind.css', array('wp-element'), '1.0', true);
 }
 
 
