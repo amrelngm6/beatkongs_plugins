3f70f28e0a7dc16b9975e3c5e779166743dd846c
unknown <amr_elngm6@yahoo.com>
2024-05-21 10:04:41 +0300

    .

--- beats-help/templates/free-beat-upload-page.php
+++ beats-help/templates/free-beat-upload-page.php
@@ -132,9 +132,11 @@ if ( $new_product ) {
                             <?php endif; ?>
                         <?php endif; ?>
                         <div class="flex">
-                            <label for="fbu-category">Category:</label>
-                            <div  class="dokan-form-group dokan-select-product-category dokan-category-open-modal" data-dokansclevel="0"  id="fbu-category-select">
+                            <div  class="w-full dokan-form-group dokan-select-product-category dokan-category-open-modal" data-dokansclevel="0"  id="fbu-category-select">
+                                <label for="fbu-category">Category:</label>
                                 <span id="dokan_product_cat_res" class="dokan-select-product-category-title dokan-ssct-level-0"><span class="dokan-selected-category-product dokan-cat-selected" id="fbu-category-text"></span></span>
+                            </div>
+                            <div  class="w-full dokan-form-group dokan-select-product-category dokan-category-open-modal" data-dokansclevel="0"  id="fbu-category-select">
                                 <span class="dokan-select-product-category-icon"><i class="fas fa-edit"></i></span>
                             </div>
                             <input type="hidden" id="fbu-category" name="fbu-category" readonly required>
