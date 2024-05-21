39c834a1b931e8c6c7fbee420c0694bb4d67b82e
unknown <amr_elngm6@yahoo.com>
2024-05-22 00:16:10 +0300

    .

--- beats-help/templates/free-beat-upload-page.php
+++ beats-help/templates/free-beat-upload-page.php
@@ -312,7 +312,7 @@ if ( $new_product ) {
 <?php do_action( 'dokan_dashboard_wrap_end' ); ?>
 <?php
 $tags = get_terms(array(
-    'taxonomy' => 'tags',
+    'taxonomy' => 'tag',
     'hide_empty' => false,
 ));
 ?>
