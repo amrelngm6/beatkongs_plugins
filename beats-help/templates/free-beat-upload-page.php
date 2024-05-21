6c4bec2629a98d3055c7d441c4285b2602e0d306
unknown <amr_elngm6@yahoo.com>
2024-05-21 09:46:27 +0300

    .

--- beats-help/templates/free-beat-upload-page.php
+++ beats-help/templates/free-beat-upload-page.php
@@ -360,7 +360,7 @@ if ( $new_product ) {
                 );
                 foreach ($list as $key => $value) {
                 ?>
-                    <li>  <?php echo json_encode($value); ?> </li>
+                    <li>  <?php echo $value; ?> </li>
                 <?php 
                 }
                 ?>
