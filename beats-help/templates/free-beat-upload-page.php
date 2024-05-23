0e35dc1f6ec75ebec7f162028ca04a3d93fbaeeb
unknown <amr_elngm6@yahoo.com>
2024-05-23 05:37:21 +0300

    .

--- beats-help/templates/free-beat-upload-page.php
+++ beats-help/templates/free-beat-upload-page.php
@@ -13,8 +13,8 @@ $category = wp_get_post_terms( $beatId, 'category');
 $station = wp_get_post_terms( $beatId, 'station');
 $moods = wp_get_post_terms( $beatId, 'mood');
 $tags = wp_get_post_terms( $beatId, 'tag');
-print_r($post);
-print_r($category);
+// print_r($post);
+// print_r($category);
 
 
 
@@ -140,7 +140,7 @@ if ( $new_product ) {
                         <div class="flex  open-modal cursor-pointer" id="open-category-modal" data-modal="#fbu-category-modal" data-text="#fbu-category-text" data-input="#fbu-category">
                             <div  class="w-full dokan-form-group dokan-select-product-category dokan-category-open-modal" data-dokansclevel="0"  id="fbu-category-g">
                                 <label for="fbu-category" class="form-label"><?php esc_html_e( 'Category', 'dokan-lite' ); ?></label>
-                                <div id="dokan_product_cat_res" class="dokan-select-product-category-title dokan-ssct-level-0"><span class="dokan-selected-category-product dokan-cat-selected text-sm font-semibold" id="fbu-category-text"><span>Select Genre</span></span></div>
+                                <div id="dokan_product_cat_res" class="dokan-select-product-category-title dokan-ssct-level-0"><span class="dokan-selected-category-product dokan-cat-selected text-sm font-semibold" id="fbu-category-text"><span><?php echo $category[0]['name'] ?? 'Select Genre'; ?></span></span></div>
                             </div>
                             <div  class="w-full " >
                                 <span class="dokan-select-product-category-icon"><i class="fas fa-edit"></i></span>
