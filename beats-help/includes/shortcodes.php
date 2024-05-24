c9aaec8535d44628c0a7e14a4d5608f96d374469
unknown <amr_elngm6@yahoo.com>
2024-05-24 04:31:56 +0300

    .

--- beats-help/includes/shortcodes.php
+++ beats-help/includes/shortcodes.php
@@ -3,10 +3,10 @@
 
 // Beats list shortcode  [beats_page]
 function dokan_beats_page_template_view() {
-    include plugin_dir_path(__FILE__) . 'templates/beats-page.php';
+    include plugin_dir_path(__FILE__) . '../templates/beats-page.php';
 }
 
 // Free Beats uploading form shortcode  [free_beat_upload_page]
 function dokan_free_beat_upload_page_template() {
-    include plugin_dir_path(__FILE__) . 'templates/free-beat-upload-page.php';
+    include plugin_dir_path(__FILE__) . '../templates/free-beat-upload-page.php';
 }
