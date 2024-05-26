12c32f9a7e5e7c59e9219c53fc4b7bb92ed86a62
unknown <amr_elngm6@yahoo.com>
2024-05-26 11:24:50 +0300

    .

--- beats-help/templates/downloadable-section.php
+++ beats-help/templates/downloadable-section.php
@@ -70,14 +70,14 @@
                             <td>
                                 <p>
                                     <input type="url" id="file-url-<?php echo $key; ?>" class="dokan-form-control dokan-w8 input_text wc_file_url" placeholder="https://" name="<?php echo $value->post_name; ?>_wc_file_url" value="<?php echo $postMeta[$value->post_name.'_wc_file_url'][0] ?? ''; ?>" style="margin-right: 8px;">
-                                    <input class="file_id" id="file-id-<?php echo $key; ?>" type="hidden" name="<?php echo $value->post_name; ?>_wc_file_id" value="">
+                                    <input class="file_id" id="file-id-<?php echo $key; ?>" type="hidden" name="<?php echo $value->post_name; ?>_wc_file_id" value="<?php echo $postMeta[$value->post_name.'_wc_file_id'][0] ?? ''; ?>">
                                     <a href="#" class="downloads_media_manager dokan-btn dokan-btn-sm dokan-btn-default upload_file_button" data-input=".file_id" data-url=".wc_file_url">Choose&nbsp;file</a>
                                 </p>
                             </td>
 
                             <td>
                                 <p>
-                                    <input type="number" id="file-price-<?php echo $key; ?>" class="dokan-form-control input_text" placeholder="Price" name="<?php echo $value->post_name; ?>_wc_file_price" value="">
+                                    <input type="number" id="file-price-<?php echo $key; ?>" class="dokan-form-control input_text" placeholder="Price" name="<?php echo $value->post_name; ?>_wc_file_price" value="<?php echo $postMeta[$value->post_name.'_wc_file_price'][0] ?? ''; ?>">
                                 </p>
                             </td>
                         </tr>
