b801f9745a525890429f88a245fbbe3aed079d92
unknown <amr_elngm6@yahoo.com>
2024-05-26 21:48:27 +0300

    .

--- beats-help/templates/beats-page.php
+++ beats-help/templates/beats-page.php
@@ -232,11 +232,10 @@ $bulk_statuses = [
                                 <td><img width="50" src="<?php echo get_the_post_thumbnail_url( $beat->ID ); ?>" /></td>
                                 <td><?php echo $beat->post_title; ?></td>
                                 <td><?php echo $beat->post_status; ?></td>
-                                <td><?php echo '0' ; ?></td>
+                                <td><?php echo strtoupper($postMeta['standard_wc_file_price'][0]) ?? '' ; ?></td>
                                 <?php  $postMeta = get_metadata( 'post', $beat->ID); ?>
                                 <td><?php echo $postMeta['_eael_post_view_count'][0] ?? '0' ; ?></td>
                                 <td><?php echo '0' ; ?></td>
-
                                 <td><?php echo strtoupper($postMeta['beat_type'][0]) ?? '' ; ?></td>
                                 <td><a href="/upload-free-beat/?beat_id=<?php echo $beat->ID; ?>">Edit</a></td>
                             </tr>
