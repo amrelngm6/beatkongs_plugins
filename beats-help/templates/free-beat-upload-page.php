1b1141b360d0d7938dc1f3f1beca4d0430fa322d
unknown <amr_elngm6@yahoo.com>
2024-05-28 02:46:02 +0300

    .

--- beats-help/templates/free-beat-upload-page.php
+++ beats-help/templates/free-beat-upload-page.php
@@ -133,7 +133,7 @@ if ( $new_product ) {
                                 <input disabled="true" style="max-width:200px" type="text"  name="beat_slug" id="beat_slug" value="<?php echo $post['post_name'] ?? ''; ?>" class="dokan-form-control valid" placeholder="Link path.." aria-invalid="false">
                                 <span id="activate-permalink">Change</span> <span style="display:none" id="validate-permalink">Validate</span> 
                             </div>
-                            <span id="ajax-span-loader"><img src="<?php echo plugin_dir_url(__FILE__); ?>assets/img/loader.gif" width="40" /></span>
+                            <span id="ajax-span-loader"><img src="<?php echo plugin_dir_url(__FILE__); ?>../assets/img/loader.gif" width="40" /></span>
                         </div>
 
                         <div id="slug-validation-message"></div>
