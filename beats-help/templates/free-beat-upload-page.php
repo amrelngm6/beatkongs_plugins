af72c686b8f0ea0ecf1b231e4d027b84bcfad355
unknown <amr_elngm6@yahoo.com>
2024-05-28 02:30:11 +0300

    .

--- beats-help/templates/free-beat-upload-page.php
+++ beats-help/templates/free-beat-upload-page.php
@@ -130,7 +130,7 @@ if ( $new_product ) {
                             <label for="bpm" class="form-label"><?php esc_html_e( 'Beat link', 'dokan-lite' ); ?></label>
                             <div class="w-full flex">
                                 <span style="padding-top:5px; font-size: 14px"><?php echo get_site_url();?></span>
-                                <input style="max-width:200px" type="text"  name="post-slug" id="beat_bpm" value="<?php echo $postMeta['beat_bpm'][0] ?? ''; ?>" class="dokan-form-control valid" placeholder="BPM (Beats per minute).." aria-invalid="false">
+                                <input style="max-width:200px" type="text"  name="beat_slug" id="beat_slug" value="<?php echo $post->post_name ?? ''; ?>" class="dokan-form-control valid" placeholder="Link path.." aria-invalid="false">
                                 <span>Validate</span>
                             </div>
                         </div>
