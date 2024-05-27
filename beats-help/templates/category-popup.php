68dd5c8ff820d000d56f37c37aff59ce20f3308d
unknown <amr_elngm6@yahoo.com>
2024-05-27 05:49:51 +0300

    .

--- beats-help/templates/category-popup.php
+++ beats-help/templates/category-popup.php
@@ -51,3 +51,58 @@
             </div>
         </div>
 
+
+<!-- The Modal -->
+<div id="dokan-product-category-modal" class="dokan-product-category-modal" style="display:flex">
+
+    <!-- Modal content -->
+    <div class="dokan-product-category-modal-content">
+        <div class="dokan-product-category-modal-header">
+            <div class="dokan-product-category-title">
+                <span class="dokan-single-title">Add new Genre</span>
+                <span class="dokan-single-des">Please choose the right Genre for this beat</span>
+            </div>
+            <div class="dokan-product-category-close">
+                <span class="close" id="dokan-category-close-modal">&times;</span>
+            </div>
+        </div>
+        <div class="dokan-product-category-modal-body">
+            <div class="dokan-category-search-container">
+                <div class="dokan-cat-search-box">
+                    <span class="dokan-cat-search-icon"><i class="fas fa-search"></i></span>
+                    <input maxlength="100" id="dokan-single-cat-search-input" class="dokan-cat-search-input" type="text" placeholder="Search Genre">
+                    <span class="dokan-cat-search-text-limit"><span id="dokan-cat-search-text-limit">0</span>/100</span>
+                </div>
+                <div id="dokan-cat-search-res" class="dokan-cat-search-res dokan-hide">
+                    <ul id="dokan-cat-search-res-ul" class="dokan-cat-search-res-ul"></ul>
+                </div>
+            </div>
+            <div class="dokan-single-categories-container">
+                <span class="dokan-single-categories-left dokan-single-categories-arrow dokan-hide">
+                    <span class="dokan-single-categories-left-box">
+                        <span><i class="fas fa-chevron-left"></i></span>
+                    </span>
+                </span>
+                <div class="dokan-single-categories" id="dokan-single-categories"></div>
+                <span class="dokan-single-categories-right dokan-single-categories-arrow dokan-hide">
+                    <span class="dokan-single-categories-right-box">
+                        <span><i class="fas fa-chevron-right"></i></span>
+                    </span>
+                </span>
+            </div>
+
+        </div>
+        <div class="dokan-product-category-modal-footer">
+            <div class="dokan-selected-category-label-container">
+                <span class="dokan-selected-category-label">Selected: </span>
+                <span class="dokan-selected-category-span" id="dokan-selected-category-span">
+                    <span class="dokan-selected-category-product">No Genres</span>
+                </span>
+            </div>
+            <div class="dokan-product-category-button-container">
+                <button class="dokan-single-cat-select-btn dokan-btn dokan-btn-default dokan-btn-theme" id="dokan-single-cat-select-btn" type='button'>Done</button>
+            </div>
+        </div>
+    </div>
+
+</div>
