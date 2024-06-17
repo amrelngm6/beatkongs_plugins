d054893f611f4be0c9bd539469feec7e47563e62
unknown <amr_elngm6@yahoo.com>
2024-06-17 18:51:39 +0300

    Fix form issue

--- mp3-music-player-by-sonaar/includes/class-sonaar-music-widget.php
+++ mp3-music-player-by-sonaar/includes/class-sonaar-music-widget.php
@@ -2848,6 +2848,7 @@ class Sonaar_Music_Widget extends WP_Widget{
             return false;
         }
 
+        return false;
         $product = wc_get_product($id);
         if ($product->is_type('variable')) {
             return true;
