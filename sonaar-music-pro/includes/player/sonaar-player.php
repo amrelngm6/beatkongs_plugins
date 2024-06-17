f310776e18ed4b2e8b0162312644e12f37ad96d6
unknown <amr_elngm6@yahoo.com>
2024-06-17 18:18:58 +0300

    Fix form issue

--- sonaar-music-pro/includes/player/sonaar-player.php
+++ sonaar-music-pro/includes/player/sonaar-player.php
@@ -72,7 +72,7 @@
         <div class="metadata">
           <div class="metadata-inner">
             <div class="track-name" @mouseover="scroll">{{list.tracks[currentTrack].track_title}}</div>
-            <div class="track-artist" @mouseover="scroll" v-html="list.tracks[currentTrack].track_artist && typeof sonaar_music.option.show_artist_name != 'undefined' ? (sonaar_music.option.artist_separator+' <a href=<?php echo get_site_url();?>/'+list.tracks[currentTrack].track_artist)+' > ' + list.tracks[currentTrack].track_artist) +'</a> ' : ''" ></div>
+            <div class="track-artist" @mouseover="scroll" > <span v-html="list.tracks[currentTrack].track_artist && typeof sonaar_music.option.show_artist_name != 'undefined' ? (sonaar_music.option.artist_separator) : ''"></span>  <a :href="'/author/'+list.tracks[currentTrack].track_artist" v-html="list.tracks[currentTrack].track_artist"></a></div>
             <div class="track-album" @mouseover="scroll" v-if="classes.show_album_title && list.tracks[currentTrack].album_title">{{list.tracks[currentTrack].album_title}}</div>
           </div>
         </div>
